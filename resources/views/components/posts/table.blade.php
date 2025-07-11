f9327973377a973b355f7bd962cb44b4277fa2e8
AlvinSanudharma <alvinsanudharma@gmail.com>
2025-07-11 17:39:53 +0800

    Modify Toast

--- resources/views/components/posts/table.blade.php
+++ resources/views/components/posts/table.blade.php
@@ -1,6 +1,6 @@
 @if (Session::has('success'))
     <div class="flex justify-center">
-        <div id="toast-success"
+        <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 900)" x-show="show" x-transition id="toast-success"
             class="absolute top-[10rem] flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow-sm dark:text-gray-400 dark:bg-gray-800"
             role="alert">
             <div
@@ -15,7 +15,7 @@
             <div class="ms-3 text-sm font-normal">{{ Session::get('success') }}</div>
             <button type="button"
                 class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-                data-dismiss-target="#toast-success" aria-label="Close">
+                data-dismiss-target="#toast-success" aria-label="Close" @click="show = false">
                 <span class="sr-only">Close</span>
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                     viewBox="0 0 14 14">
